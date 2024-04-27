f4d379c00e0e26722abc101a666c0e303148fa28
DanieII <danielbogdanov1748@gmail.com>
2024-04-27 22:12:37 +0300

    Added persistent storage

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,72 @@
-use std::io;
+use std::env;
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead, BufReader, Write};
 
 fn main() {
-    // let mut tasks: Vec<String> = Vec::new();
-    let mut tasks: Vec<String> = vec!["test1".to_string(), "test2".to_string()];
-    let mut is_active: bool = true;
+    create_file();
+    let todos = get_todos().expect("Could not read todos");
+    let args: Vec<String> = env::args().collect();
 
-    while is_active {
-        let mut choice = String::new();
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("Failed to read choice");
+    if args.len() >= 2 {
+        let action = &args[1];
+        match action.as_str() {
+            "add" => add_todo(&args),
+            _ => println!("{} is not an available action", action),
+        }
+    } else {
+        println!("doit is a cli written in rust")
+    }
+}
 
-        match choice.trim() {
-            "1" => add_task(&mut tasks),
-            "2" => show_tasks(&mut tasks),
-            _ => break,
+fn get_file_path() -> String {
+    let file_path = match env::var("HOME") {
+        Ok(path) => path + "/.doit",
+        Err(_) => ".doit".to_string(),
+    };
+
+    file_path
+}
+
+fn create_file() {
+    let file_path: String = get_file_path();
+    let file = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(&file_path);
+
+    match file {
+        Ok(_) => return,
+        Err(e) => {
+            if e.kind() != io::ErrorKind::AlreadyExists {
+                panic!("Could not create file")
+            }
         }
     }
 }
 
-fn add_task(tasks: &mut Vec<String>) {
-    let mut task_name = String::new();
-    io::stdin()
-        .read_line(&mut task_name)
-        .expect("Failed to read task name");
+fn get_todos() -> io::Result<Vec<String>> {
+    let file_path: String = get_file_path();
+    let file = File::open(file_path).expect("Could not open file");
+    let reader = BufReader::new(file);
 
-    tasks.push(task_name.clone());
-    println!("{}", task_name);
+    reader.lines().collect()
 }
 
-fn show_tasks(tasks: &mut Vec<String>) {
-    for task in tasks {
-        println!("{}", task)
+fn add_todo(args: &Vec<String>) {
+    if args.len() == 2 {
+        println!("A text for the todo must be provided as a second argument");
+        return;
     }
+
+    let todo = args[2].to_string();
+    let file_path: String = get_file_path();
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&file_path)
+        .expect("Could not open file");
+
+    let text = todo.to_string() + "\n";
+    file.write_all(text.as_bytes())
+        .expect("Could not append to file");
 }
