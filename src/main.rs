ab6762f917b991f2db6c1a0e4293abb6d917c31f
DanieII <danielbogdanov1748@gmail.com>
2024-04-28 11:59:16 +0300

    Add ls and start implementing rm

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use colored::Colorize;
 use std::env;
 use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, BufReader, Write};
 
 fn main() {
     create_file();
-    let todos = get_todos().expect("Could not read todos");
     let args: Vec<String> = env::args().collect();
 
     if args.len() >= 2 {
         let action = &args[1];
         match action.as_str() {
             "add" => add_todo(&args),
+            "ls" => list_todos(),
+            "rm" => remove_todo(&args),
             _ => println!("{} is not an available action", action),
         }
     } else {
@@ -52,6 +54,19 @@ fn get_todos() -> io::Result<Vec<String>> {
     reader.lines().collect()
 }
 
+fn list_todos() {
+    let todos = get_todos().expect("Could not read todos");
+
+    for (i, todo) in todos.iter().enumerate() {
+        let number = i + 1;
+
+        println!("{} {}", number, todo);
+        if number < todos.len() {
+            println!()
+        }
+    }
+}
+
 fn add_todo(args: &Vec<String>) {
     if args.len() == 2 {
         println!("A text for the todo must be provided as a second argument");
@@ -66,7 +81,26 @@ fn add_todo(args: &Vec<String>) {
         .open(&file_path)
         .expect("Could not open file");
 
-    let text = todo.to_string() + "\n";
+    let text = todo.trim().to_string() + "\n";
     file.write_all(text.as_bytes())
         .expect("Could not append to file");
+}
+
+fn remove_todo(args: &Vec<String>) {
+    if args.len() == 2 {
+        println!("A todo number must be provided as a second argument");
+        return;
+    }
+    if !is_string_numeric(&args[2]) {
+        println!("The second argument must be a number")
+    }
+}
+
+fn is_string_numeric(str: &String) -> bool {
+    for c in str.chars() {
+        if !c.is_numeric() {
+            return false;
+        }
+    }
+    return true;
 }
